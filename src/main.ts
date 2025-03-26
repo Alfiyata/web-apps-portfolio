7e660fe9b661a8bebbc00019896f91d237a59b78
Alfiyata <48384897+Alfiyata@users.noreply.github.com>
2025-03-27 03:39:54 +0700

    Fixing goolge analytics

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,57 @@ import 'aos/dist/aos.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import LoadingSpinner from './components/molecules/SpinnerLoading'
-
+import VueGtag, { useGtag } from 'vue-gtag-next'
 
 import App from './App.vue'
 import router from './router'
 import AOS from 'aos';
+import LoadingSpinner from './components/molecules/SpinnerLoading'
 
+const GA_MEASUREMENT_ID = 'G-VX4Q2YG7T6'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 AOS.init();
 app.component('LoadingSpinner', LoadingSpinner)
+
+app.use(VueGtag, {
+  property: {
+    id: GA_MEASUREMENT_ID
+  }
+})
+
+const pathNameArray: string[] = [];
+let isProcessing = false;
+
+router.beforeEach(async (to, from, next) => {
+  next();
+
+  if (isProcessing) {
+    return;
+  }
+
+  isProcessing = true;
+
+  await new Promise(resolve => setTimeout(resolve, 3000));
+
+  const joinPathName = window.location.pathname;
+  
+  if (pathNameArray[pathNameArray.length - 1] !== joinPathName) {
+    pathNameArray.push(joinPathName);
+  }
+
+  const finalPathName = pathNameArray[pathNameArray.length - 1];
+  const pageTitleName = to.name;
+
+  const gtag = useGtag();
+  gtag.pageview({
+    page_title: pageTitleName as string,
+    page_path: finalPathName
+  });
+
+  isProcessing = false;
+});
 
 app.mount('#app')
